16d980ea8959634d6f4560949aac1aaccfeb2660
chandni2629 <chandanilodhi5@gmail.com>
2025-01-08 23:03:18 -1200

    Add files via upload

---
+++ AssignmentOPLab16.py
@@ -0,0 +1,27 @@
+x = 10
+x += 5
+print("Addition (x += 5):", x)
+# subtraction Assignment
+x = 10 
+x -= 3
+print("(x -= 3):", x)
+#Multiplication Assignment
+x = 12
+x *= 2
+print("Multplical (x *= 2):", x)
+# Division Assignment
+x = 24
+x /= 4
+print("Division (x /= 2):",x)
+# Floor Division Assignment
+x = 3
+x //= 2
+print("Floor Division (x //= 2):", x)
+# Modulus Assignment
+x = 3.0
+x %= 3
+print("(x *= 2):", x)
+# Exponentiation Assignment 
+x = 8
+x **= 2
+print("(x **= 2):", x)
